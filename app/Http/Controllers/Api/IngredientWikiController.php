c1f01295370fdfa230b7215b531da3be737c696f
Nik Uzair <nikuzairsc@gmail.com>
2023-11-15 20:17:32 +0800

    Add HTTP response for Ingredient Wiki

--- app/Http/Controllers/Api/IngredientWikiController.php
+++ app/Http/Controllers/Api/IngredientWikiController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\IngredientWiki;
 
 class IngredientWikiController extends Controller
 {
     public function ingredientWiki()
     {
-        $ingredientWiki = \App\Models\IngredientWiki::select('id', 'name', 'thumbnail', 'description', 'seasonality', 'storage', 'cooking_tips', 'health_benefits')->get();
-        return response()->json($ingredientWiki);
+        $result = IngredientWiki::select('id', 'name', 'thumbnail', 'description', 'seasonality', 'storage', 'cooking_tips', 'health_benefits')->get();
+
+        return response() -> json([
+            'code' => 200,
+            'msg' => 'success',
+            'data' => $result
+        ], 200);
     }
 
 }
